89bb86735bdf8deef86b0a22e3487648b25b4d42
Ose-wizerd <osamadawud5@gmail.com>
2024-11-21 22:02:45 +0300

    continued editing

--- hello.java
+++ hello.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class hello {
   public static void main(String[] args){
-       System.out.println("hello woeld");
-       int x=34;
-       int y=3;
-       System.out.println(x*y);
-  String x="ok";
-  String y="no";
-  String temp=x;
-  x=y;
-  y=temp;
+//        System.out.println("hello woeld");
+//        int x=34;
+//        int y=3;
+//        System.out.println(x*y);
+//   String x="ok";
+//   String y="no";
+//   String temp=x;
+//   x=y;
+//   y=temp;
 
-  System.out.println(x);
-  System.out.println(y);
+//   System.out.println(x);
+//   System.out.println(y);
   
   Scanner take = new Scanner(System.in);
 
@@ -24,7 +24,11 @@ public class hello {
   System.out.print("where is your home: ");
   take.nextLine();
   String home= take.nextLine();
-
+// Scanner scanner = new Scanner(System.in);
+// Scanner take = new Scanner(System.in);
+// System.out.print("Enter your name: ");
+// String name=take.nextLine();
+// System.out.println("hello "+ name);
 
 
    }
