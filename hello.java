dd08a16185313696ecc95043c0884c8ba5fa2248
Ose-wizerd <osamadawud5@gmail.com>
2024-11-21 21:45:57 +0300

    starting java

---
+++ hello.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class hello {
+  public static void main(String[] args){
+       System.out.println("hello woeld");
+       int x=34;
+       int y=3;
+       System.out.println(x*y);
+  String x="ok";
+  String y="no";
+  String temp=x;
+  x=y;
+  y=temp;
+
+  System.out.println(x);
+  System.out.println(y);
+  
+  Scanner take = new Scanner(System.in);
+
+  System.out.println("what is  your name: ");
+  String name=take.nextLine();
+  System.out.println("hello " + name+ "how old are you: ");
+  int age=take.nextInt();
+  System.out.print("where is your home: ");
+  take.nextLine();
+  String home= take.nextLine();
+
+
+
+   }
+} 
