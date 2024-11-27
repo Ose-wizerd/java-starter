b7564c23ae25ec60bc7bc83fcd57635c9a6fbdfb
Ose-wizerd <osamadawud5@gmail.com>
2024-11-27 21:44:08 +0300

    daily

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
-public class hello {
-  public static void main(String[] args){
+
 //        System.out.println("hello woeld");
 //        int x=34;
 //        int y=3;
@@ -35,18 +34,163 @@ public class hello {
 // int age = Integer.parseInt(JOptionPane.showInputDialog("how old you"));
 // JOptionPane.showMessageDialog(null, name + "you are "+ age + "years age!");
 
-Scanner take= new Scanner(System.in);
+// Scanner take= new Scanner(System.in);
 
-System.out.println("enter your age: ");
-int age= take.nextInt();
-take.nextLine();
+// System.out.println("enter your age: ");
+// int age= take.nextInt();
+// take.nextLine();
 
-System.out.println("enter your name");
-String name=take.nextLine();
+// System.out.println("enter your name");
+// String name=take.nextLine();
 
-JOptionPane.showMessageDialog(null, "hello "+ name);
-JOptionPane.showMessageDialog(null, "hello "+ name + "you are " + age + "years old");
+// JOptionPane.showMessageDialog(null, "hello "+ name);
+// JOptionPane.showMessageDialog(null, "hello "+ name + "you are " + age + "years old");
+
+// Scanner take= new Scanner(System.in);
+
+// System.out.println("enter your age: ");
+// int age= take.nextInt();
+// take.nextLine();
+
+// System.out.println("enter your name");
+// Str
+// Scanner take= new Scanner(System.in);
+
+// System.out.println("enter your age: ");
+// int age= take.nextInt();
+// take.nextLine();
+
+// System.out.println("enter your name");
+// String name=take.nextLine();
+
+// JOptionPane.showMessageDialog(null, "hello "+ name);
+// JOptionPane.showMessageDialog(null, "hello "+ name + "you are " + age + "years old");
+// ing name=take.nextLine();
+
+// JOptionPane.showMessageDialog(null, "hello "+ name);
+// JOptionPane.showMessageDialog(null, "hello "+ name + "you are " + age + "years old");
 
 
-   }
-} 
+
+
+// class student{
+//   String name;
+//   int age;
+
+  
+// }
+
+// public class hello{
+
+//   public static void main(String[] agrs){
+//     student stud1= new student();
+//     student stud2=new student();
+//     System.out.println(stud1.age);
+//     stud1.name="osama";
+//     stud1.age=22;
+//     stud2.name="cala";
+//     stud2.age=24;
+//     System.out.println(stud2.name);
+//     System.out.println(stud1.name);
+
+
+//   }
+
+// }
+
+// class Box{
+//   double width;
+//   double height;
+//   double depth;
+
+// }
+// public class hello{
+//   public static void main(String[] args){
+//     Box box1= new Box();
+//     Box box2=new Box();
+//     box1.width=10;
+//     box1.height=20;
+//     box1.depth=3;
+//     box2.width=15;
+//     box2.depth=10;
+//     box2.height=5;
+//     double vol1=box1.depth*box1.height*box1.width;
+//     double vol2=box2.depth*box2.height*box2.width;
+//     System.out.println("this is volume1: "+vol1);
+//     System.out.println("this is volume2: "+vol2);
+
+//   }
+// }
+
+public class hello{
+  public static void main(String[] args){
+    // System.out.println("hello" +"world");
+    // String name=JOptionPane.showInputDialog("enter your name");
+    // int age=Integer.parseInt(JOptionPane.showInputDialog("enter your age"));
+    // JOptionPane.showMessageDialog(null, "hello " + name+ " you are "+age + " years old!");
+Scanner scanner=new Scanner(System.in);
+
+//     System.out.println("enter value of x: ");
+//     double y=inp.nextInt();
+//     System.out.println("enter value of y: ");
+//     double x=inp.nextInt();
+//     double z=Math.sqrt(x*y);
+// System.out.println("the value of Z is: "+ z);
+// System.out.println("enter your name: ");
+// String name=inp.nextLine();
+// switch(name){
+//   case "osama": System.out.println("you are 1st on the list!");
+//   break;
+//   case "cala":System.out.println("you are 2nd on the list!");
+//   break;
+//   case "husen":System.out.println("you are 3rd on the list!");
+//   break;
+//   default:System.out.println("you are not on the list!");
+//   break;
+// }
+// System.out.println("enter number of row: ");
+// int row=scanner.nextInt();
+// System.out.println("enter number of column: ");
+// int column=scanner.nextInt();
+// System.out.println("enter your symbol: ");
+// String symbol=scanner.next();
+
+// for(int i=1; i<=row;i++){
+//   System.out.println();
+//   for(int j=1; j<=i;j++){
+//     System.out.print(symbol);
+//   }
+// }
+
+// String[] names={"osama","cala","loko","balanbel"};
+// System.out.println(names[2]);
+// String[][] names={{"umma", "abba"},{"kani","lello"},{"ismu","yakubo"},{"imru","ibro"},{"shueyb","ayub"},{"salman","yunis"}};
+// // names[0]="cala";
+// // names[1]="husen";
+// // names[2]="alo";
+// // names[3]="feri";
+// for(int i=0; i<names.length;i++){ 
+//   System.out.println();
+//   for(int j=0;j<names[i].length; j++){  
+// System.out.println(names[i][j] + " ");
+// }
+// }
+
+String name="osama";
+// boolean reslt=name.equalsIgnoreCase("Osama");
+// char reslt =name.charAt(1);
+//  boolean reslt =name.isEmpty();
+// String reslt=name.toUpperCase();
+// String reslt=name.toLowerCase();
+// String reslt=name.replace("o","a");
+
+
+
+System.out.println(reslt);
+
+
+
+
+
+  }
+}
